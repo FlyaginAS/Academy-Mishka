35dfbd3320544a1091232bcfcc1a2a78019599ac
ruscat <rus.reg.36@gmail.com>
2019-09-07 12:39:21 +0300

    добавил скрипт для модального окна

--- dev/js/main.js
+++ dev/js/main.js
@@ -6,10 +6,23 @@ let modal = document.querySelector('.modal-cart');
 let btn =document.querySelector('.featured__btn ');
 let svg_close=document.querySelector('.main-nav__toggle-svg-close');
 let svg_open = document.querySelector('.main-nav__toggle-svg-open');
-
+let
+// закрытие меню
 toggle.addEventListener('click', function () {
     menu.classList.toggle('visually-hidden');
     svg_close.classList.toggle('visually-hidden');
     svg_open.classList.toggle('visually-hidden');
 });
+//открытие модального окна
+btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    overlay.classList.toggle('visually-hidden');
+    modal.classList.toggle('visually-hidden');
+});
+//закрытие модального окна при щелчке на оверлее
+overlay.addEventListener('click', function () {
+    overlay.classList.toggle('visually-hidden');
+    modal.classList.toggle('visually-hidden');
+});
+
 
