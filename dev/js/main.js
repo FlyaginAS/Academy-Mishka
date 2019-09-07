bfd4b5cb0bfac884f43fe5b9f57847b61e058b3a
ruscat <rus.reg.36@gmail.com>
2019-09-07 12:18:32 +0300

    добдавил на кнопку закрытия скрипт на всек страницы

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+let toggle= document.querySelector('.main-nav__toggle');
+let menu = document.querySelector('.list-main');
+let overlay=document.querySelector('.modal-overlay');
+let modal = document.querySelector('.modal-cart');
+let btn =document.querySelector('.featured__btn ');
+let svg_close=document.querySelector('.main-nav__toggle-svg-close');
+let svg_open = document.querySelector('.main-nav__toggle-svg-open');
+
+toggle.addEventListener('click', function () {
+    menu.classList.toggle('visually-hidden');
+    svg_close.classList.toggle('visually-hidden');
+    svg_open.classList.toggle('visually-hidden');
+});
+
