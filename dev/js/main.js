a82eb2486441758b60449e6aed2797165a9de6dd
ruscat <rus.reg.36@gmail.com>
2019-09-11 16:11:27 +0300

    настройка автомитазации с сервером поправил пути картинок

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,7 +3,7 @@ let toggle= document.querySelector('.main-nav__toggle');
 let menu = document.querySelector('.list-main');
 let overlay=document.querySelector('.modal-overlay');
 let modal = document.querySelector('.modal-cart');
-let btn =document.querySelector('.featured__btn ');
+let btn =document.querySelector('.featured__btn');
 let svg_close=document.querySelector('.main-nav__toggle-svg-close');
 let svg_open = document.querySelector('.main-nav__toggle-svg-open');
 
